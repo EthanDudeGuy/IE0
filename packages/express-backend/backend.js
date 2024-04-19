64d16e3549ac959916e78b7d372ddbff1a590cea
EthanDudeGuy <ethan.emery01@gmail.com>
2024-04-19 13:34:38 -0700

    linked to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -64,6 +65,7 @@ const deleteUser = (id) => {
     }
 }
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
